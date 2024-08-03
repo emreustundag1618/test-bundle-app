150a8f843d41761fea27b2ad94c920002d9014c0
emreustundag1618 <emre.ustundag.1.618@gmail.com>
2024-08-03 15:40:23 +0300

    Change color modal implementation but styles still remain

--- extensions/custom-bundle/assets/custom_bundle.js
+++ extensions/custom-bundle/assets/custom_bundle.js
@@ -7,8 +7,9 @@ class CustomBundleApp extends HTMLElement {
         this.shopifyDomain = "emre-development-store.myshopify.com";
         this.storefrontAccessToken = "2febbc203fcc4f4db6e40969eeafdb64"
         this._bundleData = []; // this will be all data to be fetched remix api
-        this._shopifyData = []; // this will be all data to be fetched from shopify via storefront api
-        this.transformedData = []; // this will include a list of variants to post cart/add for add to cart functionality {id: 123423423, quantity: 2}
+        this.transformedData = []; // this will include a list of variants to post cart/add for add to cart functionality
+        this.choosenData = []; // this will include a list of variants to post cart/add for add to cart functionality 
+        // {id: variantId, quantity: selectedQuantity, quantityNeeded: neededQuantity, inventory: availableForSale}
         // TODO: transformedData will be mutated by selecting another variant which is fetched by clicking change color button and opening a modal to choose another variant.
         this.productId = this.dataset.product_id;
         this.endpoint = baseUrl + "/api/bundles?shopifyId=" + this.productId;
@@ -16,6 +17,8 @@ class CustomBundleApp extends HTMLElement {
         this.style.fontSize = "14px";
         this.style.lineHeight = 1.2;
 
+        this.selectedProductId = "";
+        this.selectedVariantId = "";
         this.modalOpen = false;
         console.log("Bundle product id: ", this.productId);
     }
@@ -33,14 +36,34 @@ class CustomBundleApp extends HTMLElement {
         await this.getBundleData();
         await this.getProductsAndVariantsFromShopify(this._bundleData.products.map(product => product.proId.split("/").pop()));
         this.renderBundle();
-        document.getElementById('sensy-add-to-cart').addEventListener('click', () => this.addToCart(this._bundleData.products));
-
-        console.log("Bundle data: ", this._bundleData)
+        console.log("Bundle data: ", this._bundleData);
         mutateData(this._bundleData, this.transformedData);
-        console.log(this.transformedData)
+        console.log("Transformed data(final): ", this.transformedData);
+        // initialize modal
+        this.initializeModal();
 
-        // test
-        
+        const modal = document.getElementById('custom-bundle-modal');
+
+        document.getElementById('sensy-add-to-cart').addEventListener('click', () => this.addToCart(this.transformedData));
+        const changeColorButtons = document.querySelectorAll('.custom-bundle-change-color');
+        changeColorButtons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                modal.style.display = 'block';
+                this.selectedVariantId = e.target.dataset.varId;
+                this.selectedProductId = e.target.parentElement.dataset.proId;
+                this.updateModalContent()
+            })
+        });
+        const closeModalButton = document.getElementById('closeModalButton');
+        closeModalButton.addEventListener('click', () => {
+            modal.style.display = 'none'
+        });
+
+        window.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                modal.style.display = 'none'
+            }
+        });
     }
 
     async getProductsAndVariantsFromShopify(productIds) {
@@ -156,8 +179,9 @@ class CustomBundleApp extends HTMLElement {
                                         <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true"><path d="M12.5 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path><path fill-rule="evenodd" d="M9.018 3.5h1.964c.813 0 1.469 0 2 .043.546.045 1.026.14 1.47.366a3.75 3.75 0 0 1 1.64 1.639c.226.444.32.924.365 1.47.043.531.043 1.187.043 2v1.964c0 .813 0 1.469-.043 2-.045.546-.14 1.026-.366 1.47a3.75 3.75 0 0 1-1.639 1.64c-.444.226-.924.32-1.47.365-.531.043-1.187.043-2 .043h-1.964c-.813 0-1.469 0-2-.043-.546-.045-1.026-.14-1.47-.366a3.75 3.75 0 0 1-1.64-1.639c-.226-.444-.32-.924-.365-1.47-.043-.531-.043-1.187-.043-2v-1.964c0-.813 0-1.469.043-2 .045-.546.14-1.026.366-1.47a3.75 3.75 0 0 1 1.639-1.64c.444-.226.924-.32 1.47-.365.531-.043 1.187-.043 2-.043Zm-1.877 1.538c-.454.037-.715.107-.912.207a2.25 2.25 0 0 0-.984.984c-.1.197-.17.458-.207.912-.037.462-.038 1.057-.038 1.909v1.428l.723-.867a1.75 1.75 0 0 1 2.582-.117l2.695 2.695 1.18-1.18a1.75 1.75 0 0 1 2.604.145l.216.27v-2.374c0-.852 0-1.447-.038-1.91-.037-.453-.107-.714-.207-.911a2.25 2.25 0 0 0-.984-.984c-.197-.1-.458-.17-.912-.207-.462-.037-1.056-.038-1.909-.038h-1.9c-.852 0-1.447 0-1.91.038Zm-2.103 7.821a7.12 7.12 0 0 1-.006-.08.746.746 0 0 0 .044-.049l1.8-2.159a.25.25 0 0 1 .368-.016l3.226 3.225a.75.75 0 0 0 1.06 0l1.71-1.71a.25.25 0 0 1 .372.021l1.213 1.516c-.021.06-.045.114-.07.165-.216.423-.56.767-.984.983-.197.1-.458.17-.912.207-.462.037-1.056.038-1.909.038h-1.9c-.852 0-1.447 0-1.91-.038-.453-.037-.714-.107-.911-.207a2.25 2.25 0 0 1-.984-.984c-.1-.197-.17-.458-.207-.912Z"></path></svg>
                                         ` }
                                     </div>
-                                    <div class="product-info">
-                                        <div class="variant-name">${variant.title}</div>
+                                    <div class="product-info" data-pro-id="${product.proId.split('/').pop()}">
+                                        ${product.productType !== "accessories" ? `<span class="custom-bundle-change-color" data-var-id="${variant.varId.split('/').pop()}">Change color</span>` : ``}
+                                        <div class="variant-name">${variant.title} </div>
                                         <div class="product-name">${product.title}</div>
                                         ${variant.inventory === 0 ? `<span>Out of stock</span>` : ""}
                                         ${variant.quantityNeeded < variant.inventory ? `<span>In stock</span>` : ""}
@@ -200,13 +224,13 @@ class CustomBundleApp extends HTMLElement {
         products.forEach(product => {
             product.variants.forEach(variant => {
                 // Check it has variant or product (because shopify has a weird structure when no variant in a product)
-                if (variant.title !== "Default Title") { // then it has NO variant
-
+                if (variant.quantityNeeded) { // then it has NO variant
+                    formData.items.push({
+                        'id': variant.id.split('/').pop(),
+                        'quantity': variant.quantityNeeded
+                    });
                 }
-                formData.items.push({
-                    'id': variant.varId.split('/').pop(),
-                    'quantity': 1
-                });
+
             });
         })
 
@@ -227,10 +251,71 @@ class CustomBundleApp extends HTMLElement {
             });
     }
 
-    renderModal(productId) {
+    initializeModal() {
 
+        let modalHtml = `
+            <div id="custom-bundle-modal" class="modal" style="display:${this.modalOpen ? "block" : "none"}">
+                <div class="modal-content">
+                    <span id="closeModalButton" class="close-button">&times;</span>
+                    <div id="customBundleModalContent">Loading...</div>
+                </div>
+            </div>
+        `
+        document.body.insertAdjacentHTML("beforeend", modalHtml);
     }
 
+    updateModalContent() {
+        console.log("updateModalContent called. ProductId is: ", this.selectedProductId);
+        const selectedProduct = this.transformedData.filter(product => product.id.split('/').pop() === this.selectedProductId)[0];
+        const selectedVariant = this.transformedData
+            .filter(product => product.id.split('/').pop() === this.selectedProductId)
+            .map(product => {
+                return {
+                    ...product,
+                    variants: product.variants.filter(variant => variant.id.split('/').pop() === this.selectedVariantId)
+                };
+            })
+            .filter(product => product.variants.length > 0)[0].variants[0];
+
+        console.log("Selected product: ", selectedProduct);
+        console.log("Selected variant: ", selectedVariant);
+
+        const modalContent = document.getElementById('customBundleModalContent');
+        if (this.selectedProductId) {
+            modalContent.innerHTML = `  <div id="modalTitle">Select Your Color</div>
+                                        <div id="available-colors">Available Colors</div>
+                                        <div id="selected-color">Selected variant id: ${this.selectedVariantId}</div>
+                                        <div id="selected-color">Color: Light Pink</div>
+                                        <div id="modalColorList">
+                                        ${selectedProduct.variants.map(variant => {
+                                            return `<div class="custom-bundle-modal-image-container">
+                                                        <img class="custom-bundle-variant-img ${variant.id === selectedVariant.id ? "selected-variant": ""}"
+                                                        src="${variant.image.url}" />
+                                                        <div>${variant.title}</div>
+                                                    </div>
+                                                    `
+                                        }).join('')}</div>
+                                    `
+        } else {
+            modalContent.innerHTML = `<p>Product not found.</p>`
+        }
+
+        const modalImages = document.querySelectorAll('.custom-bundle-variant-img')
+        
+        modalImages.forEach(img => 
+            img.addEventListener('mouseenter', (e) => {
+                modalImages.forEach(img => img.classList.contains('hovered-variant') ? img.classList.remove('hovered-variant') : null)
+                e.target.classList.add('hovered-variant');
+
+            }));
+        modalImages.forEach(img => img.addEventListener('mouseleave', (e) => {
+            e.target.classList.remove('hovered-variant');
+        }));
+        modalImages.forEach(img => img.addEventListener('click', (e) => {
+            modalImages.forEach(img => img.classList.contains('selected-variant') ? img.classList.remove('selected-variant') : null)
+            e.target.classList.add('selected-variant');
+        }));
+    }
 }
 
 customElements.define("sensy-custom-bundle", CustomBundleApp);
@@ -250,7 +335,7 @@ function transformShopifyData(data) {
     })
 }
 
-function mutateData(bundle, transformedData){
+function mutateData(bundle, transformedData) {
     bundle.products.forEach(bundleProduct => {
         bundleProduct.variants.forEach(bundleVariant => {
             transformedData.forEach(transformedProduct => {
